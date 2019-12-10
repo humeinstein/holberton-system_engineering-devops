323f26fda8709973a4b41763113987040f3d258c
MacBook <macbook@MacBooks-MacBook-Pro.local>
2019-12-09 22:29:26 -0500

    checl

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
 # create file in tmp 
 
 file { '/tmp/holberton':
+    ensure  => 'present',
     path    => '/tmp/holberton',
     mode    => '0744',
     owner   => 'www-data',
     group   => 'www-data',
     content => 'I love Puppet',
-    ensure  => 'present',
 }
