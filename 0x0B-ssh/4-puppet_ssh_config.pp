069930b5437639164cb221cfe3556a823131299f
humeinste <humeincreations@gmail.com>
2019-12-11 00:31:00 +0000

    try

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,11 +1,11 @@
 # turn off pass and declare identity file
 
-File_line { 'passwd auth':
+file_line { 'passwd auth':
           path    => '/etc/ssh/ssh_config',
           line    => 'PasswordAuthentication no',
 }
 
-File_line { 'identity file':
+file_line { 'identity file':
           path       => '/etc/ssh/ssh_config',
           line       => 'IdentityFile ~/.ssh/holberton',
 }
