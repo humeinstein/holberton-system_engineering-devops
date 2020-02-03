46840fd1d0813d9e217529f6ccd7286423a39a1b
humeinste <humeincreations@gmail.com>
2020-02-03 22:05:46 +0000

    INIT: script uses api to find and print user with completed task list

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" return info on employees """
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    erequest = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                            .format(sys.argv[1]))
+   
+    name = erequest.json().get('name')
+    trequests = requests.get('https://jsonplaceholder.typicode.com/todos')
+    tasks = trequests.json()
+    taskcompleted = 0
+    totaltasks = 0
+    tasklist = []
+    for taskcheck in tasks:
+        if taskcheck.get("completed"):
+            tasklist.append(taskcheck)
+            taskcompleted += 1
+        totaltasks += 1
+    print("Employee {} is done with tasks({}/{}):".format
+          (name, taskcompleted, totaltasks))
+    for completedtasks in tasklist:
+        print("\t {}".format(completedtasks.get("title")))
+    
+
+
+
