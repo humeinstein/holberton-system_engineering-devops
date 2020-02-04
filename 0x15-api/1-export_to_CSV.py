050347d26bbe13881c31ec0989ef69a7aa20e36f
humeinste <humeincreations@gmail.com>
2020-02-04 00:57:23 +0000

    INIT: to vsv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3                                                                                     
+#!/usr/bin/python3
 """ return info on employees """
 
 if __name__ == "__main__":
@@ -8,22 +8,26 @@ if __name__ == "__main__":
     import sys
 
     todorequest = req.get("https://jsonplaceholder.typicode.com/todos",
-                     params={"userId": int(sys.argv[1])})
+                          params={"userId": int(sys.argv[1])})
     userstodocontent = todorequest.json()
     userrequest = req.get("https://jsonplaceholder.typicode.com/users",
                           params={"id": int(sys.argv[1])})
-    
+
     userinfo = userrequest.json()
     username = userinfo[0].get("username")
-    
+
     for change in userstodocontent:
         change["username"] = username
         change.pop("id")
 
     with open('{}.csv'.format(sys.argv[1]), 'w', newline='') as csvf:
-        writer = csv.DictWriter(csvf, fieldnames=["userId", "username", "completed", "title"],
-                                quoting=csv.QUOTE_ALL)
+        writer = csv.DictWriter(
+            csvf,
+            fieldnames=[
+                "userId",
+                "username",
+                "completed",
+                "title"],
+            quoting=csv.QUOTE_ALL)
         for row in userstodocontent:
             writer.writerow(row)
-    
-    
