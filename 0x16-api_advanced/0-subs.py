eddb718ca59b76f187d66c634a2f267a7656f762
humeinste <humeincreations@gmail.com>
2020-02-06 22:53:03 +0000

    INIT: returns number of subscribers or 0 if none

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" script returns amount of subscribers of given subreddit """
+
+import requests as req
+from sys import argv
+
+
+def number_of_subscribers(subreddit):
+    """ gets number of subs to subreddit """
+    url = "https://api.reddit.com/r/{}/about".format(
+        subreddit)
+    headers = {
+        'User-Agent': 'macOS:macbook.pro: by (unhiredSoftwareDude)',
+        'From': '824@holbertonschool.com'
+    }
+    grab_content = req.get(url, headers=headers).json()
+
+    if 'subscribers' in grab_content['data']:
+        subscribers = grab_content['data']['subscribers']
+        return(subscribers)
+    else:
+        return(0)
