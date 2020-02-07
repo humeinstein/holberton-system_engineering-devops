b97cf5ac88abc3964d6f0d24cd345515fb1fb072
humeinste <humeincreations@gmail.com>
2020-02-07 00:43:33 +0000

    INIT: working

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" return nunmber of subscribers """
+import requests as req
+
+
+def top_ten(subreddit):
+    """ prints first 10 titles """
+    url = 'https://api.reddit.com/r/{}/hot'.format(subreddit)
+    headers = {
+        'User-Agent': 'unhiredSoftwareDude',
+        'From': '824@holbertonschool.com'
+    }
+    get_posts = req.get(url, headers=headers).json()
+
+    for iter in range(10):
+        hot_post = get_posts['data']['children'][iter]
+        print(hot_post['data']['title'])
